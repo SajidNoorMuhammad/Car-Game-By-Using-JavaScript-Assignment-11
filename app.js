e2b870dc918f185475f9dcf2c16fdbd2dc477d87
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-05-25 01:23:03 +0500

    initialize variable.

---
+++ app.js
@@ -0,0 +1,18 @@
+let car = document.getElementById("image");
+var blank= "";
+
+// function goCar() {
+//     let count = 0;
+
+//     interval = setInterval(function abc() {
+//         count += 5;
+//         car.style.left = count + "px";
+//     },70)
+// }
+
+// function stopCar(){
+//     clearInterval(interval);
+// }
+
+
+
