48fd313cffd2cbd421e11ddbf63efcd8e4a145f1
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-05-25 01:23:33 +0500

    create a function to start a car

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let car = document.getElementById("image");
 var blank= "";
 
-// function goCar() {
-//     let count = 0;
+function goCar() {
+    let count = 0;
 
-//     interval = setInterval(function abc() {
-//         count += 5;
-//         car.style.left = count + "px";
-//     },70)
-// }
+    interval = setInterval(function abc() {
+        count += 5;
+        car.style.left = count + "px";
+    },70)
+}
 
 // function stopCar(){
 //     clearInterval(interval);
